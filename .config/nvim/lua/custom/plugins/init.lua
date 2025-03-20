f760b16c06783054d8465e925bda054459c8a45e
vanadium23 <chernoffivan@gmail.com>
2025-03-20 17:31:17 +0300

    brave new world on wayland & niri

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,14 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  -- Conjure!
+  'Olical/conjure',
+
+  -- Structural editing, optional
+  'guns/vim-sexp',
+  'tpope/vim-sexp-mappings-for-regular-people',
+  'tpope/vim-repeat',
+  'tpope/vim-surround',
+}
